4faf5d9c3fd194a3587d5cac0c9390fbd5633cbe
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-08-05 15:26:44 +0700

    Feat(bookmark): Add Bookmark model

--- models/bookmark.model.js
+++ models/bookmark.model.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 
+
+const BookSchema = new mongoose.Schema(
+    {
+        job: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Job",
+        },
+        userId: { type: String, required: true, },
+
+    }, { timestamps: true }
+);
+module.exports = mongoose.model("Bookmark", BookSchema)
