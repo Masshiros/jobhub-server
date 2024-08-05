2364339ae1bb2ec0e2fa086fa37be1a86f8a8741
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-08-05 15:15:31 +0700

    Feat(chore): Set up DB

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected"))
+  .catch((e) => console.log(e));
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port || 3000, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
