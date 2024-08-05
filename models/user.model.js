a294beab5f6a6e2e08da8ce781209bddef31f31b
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-08-05 15:21:14 +0700

    Feat(user): Add user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  location: { type: String, required: false },
+  isAdmin: { type: Boolean, default: false },
+  isAgent: { type: Boolean, default: false },
+  skills: { type: Array, default: false },
+  profile: {
+    type: String,
+    require: true,
+    default:
+      "https://d326fntlu7tb1e.cloudfront.net/uploads/4821d814-ac87-4b22-aa80-ac7336916c9a-403017_avatar_default_head_person_unknown_icon.png",
+  },
+});
+module.exports = mongoose.model("User", UserSchema);
