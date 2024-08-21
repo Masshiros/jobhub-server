783185456827e70c77c77e1602a04a79b49083d9
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-08-21 10:27:52 +0700

    Feat(chat): set up socket io

--- index.js
+++ index.js
@@ -17,6 +17,55 @@ app.use(express.json());
 app.use("/api/", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/job", jobRoute);
-app.listen(port || 3000, () =>
+const server = app.listen(port || 3000, () =>
   console.log(`Example app listening on port ${port}!`)
 );
+const io = require("socket.io")(server, {
+  pingTimeOut: 600,
+  cors: {
+    origin: "https://jobhub-server-production.up.railway.app",
+  },
+});
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+  socket.on("setup", (userId) => {
+    socket.join(userId);
+    socket.broadcast.emit("online-user", userId);
+  });
+  socket.on("typing", (room) => {
+    console.log("typing");
+    console.log("room");
+    socket.to(room).emit("typing", room);
+  });
+  socket.on("stop typing", (room) => {
+    console.log("stop typing");
+    console.log("room");
+    socket.to(room).emit("stop typing", room);
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined: " + room);
+  });
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+    var room = chat._id;
+    var sender = newMessageReceived.sender;
+    if (!sender || sender._id) {
+      console.log("Sender not defined");
+      return;
+    }
+    var senderId = sender._id;
+    console.log(senderId + "message sender");
+    const users = chat.users;
+    if (!users) {
+      console.log("User not defined");
+      return;
+    }
+    socket.to(room).emit("message received", newMessageReceived);
+    socket.to(room).emit("message sent", "New Message");
+  });
+  socket.off("setup", () => {
+    console.log("user offline");
+    socket.leave(userId);
+  });
+});
